401bf963bc2a2d5ba765184ac1c9a509b8b7d2de
pablogv63 <pablopoligv@gmail.com>
2020-11-19 20:08:33 +0100

    P - Clase credencial, campos añadidos

--- app/src/main/java/com/pablogv63/quicklock/Credential.kt
+++ app/src/main/java/com/pablogv63/quicklock/Credential.kt
@@ -1,6 +1,7 @@
 package com.pablogv63.quicklock
 
 import com.pablogv63.quicklock.Utilidades
+import java.security.MessageDigest
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
@@ -34,15 +35,13 @@ class Credential (var name: String, var category: String = "None"){
     //Constructor secundario: Leer desde archivo
     constructor(name: String, category: String,
                 lastViewed: String, lastChanged: String,
-                fields: ArrayList<CredentialField>){
-        this.name = name
-        this.category = category
+                fields: ArrayList<CredentialField>) : this(name,category) {
         this.lastViewed = lastViewed
         this.lastChanged = lastChanged
     }
 
     //Constructor residual con icono
-    constructor(icon: Int, name:String){
+    constructor(icon: Int, name:String) : this(name,name){
         println("Bad constructor called")
     }
 
@@ -56,8 +55,17 @@ open class CredentialField(val name: String, var value: String){}
 class CredentialPassword(name: String = "password", value: String) : CredentialField(name, value) {
 
     init {
-        //TODO: Manipulamos la contraseña para hashearla
+        //Manipulamos la contraseña para hashearla
+        //Si se hashea una contraseña custom no se podrá recuperar
+        //this.value = value.sha256()
+    }
 
+    //Extendemos localmente la clase String para añadirle hasheo a sha256
+    fun String.sha256(): String{
+        val bytes = this.toByteArray()
+        val md = MessageDigest.getInstance("SHA-256")
+        val digest = md.digest(bytes)
+        return digest.fold("", {str, it -> str + "%02x".format(it)})
     }
 }
 
