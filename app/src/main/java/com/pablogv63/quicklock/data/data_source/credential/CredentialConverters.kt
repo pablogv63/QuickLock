669b8c88d86f68e6edab514af44ade06335b1ca4
pablogv63 <pablopoligv@gmail.com>
2022-05-17 13:52:16 +0200

    Add QuickLock theme from Material Theme Builder
    -Add QuickLock theme made in Material Theme Builder
    -Add sample function to test theme in MainActivity

--- app/src/main/java/com/pablogv63/quicklock/data/data_source/credential/CredentialConverters.kt
+++ app/src/main/java/com/pablogv63/quicklock/data/data_source/credential/CredentialConverters.kt
@@ -1,8 +1,6 @@
 package com.pablogv63.quicklock.data.data_source.credential
 
 import androidx.room.TypeConverter
-import com.google.gson.Gson
-import com.pablogv63.quicklock.domain.model.Category
 import java.time.LocalDate
 
 class CredentialConverters {
--- app/src/main/java/com/pablogv63/quicklock/ui/MainActivity.kt
+++ app/src/main/java/com/pablogv63/quicklock/ui/MainActivity.kt
@@ -3,55 +3,81 @@ package com.pablogv63.quicklock.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.pablogv63.quicklock.ui.theme.ComposeRoomTemplateTheme
-import org.koin.androidx.viewmodel.ext.android.getViewModel
+import com.pablogv63.quicklock.ui.theme.QuickLockTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeRoomTemplateTheme {
+            QuickLockTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize()
                 ) {
-                    val viewModel = getViewModel<MainViewModel>()
-                    BDList(viewModel = viewModel)
+                    MainScreen()
                 }
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun BDList(
-    viewModel: MainViewModel
-){
-    // Obtain the data
-    val state = viewModel.state.value
+fun MainScreen() {
 
+    var selectedItem by remember { mutableStateOf(0) }
+    val items = listOf("Home", "Settings", "Favorites")
+    val icons = listOf(
+        Icons.Filled.Home, Icons.Filled.Settings,
+        Icons.Filled.Favorite)
 
-}
+    Column(
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+        SmallTopAppBar(title = { Text("ThemeDemo") }, scrollBehavior = null)
+
+        Button(onClick = { }) {
+            Text("MD3 Button")
+        }
+
+        Text("A Theme Demo")
+
+        FloatingActionButton(onClick = { }) {
+            Text("FAB")
+        }
+
+        NavigationBar {
+            items.forEachIndexed { index, item ->
+                NavigationBarItem(
+                    icon = { Icon(icons[index], contentDescription = null) },
+                    label = { Text(item) },
+                    selected = selectedItem == index,
+                    onClick = { selectedItem = index }
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ComposeRoomTemplateTheme {
-        Greeting("Android")
+    QuickLockTheme(true) {
+        Surface {
+            MainScreen()
+        }
     }
 }
